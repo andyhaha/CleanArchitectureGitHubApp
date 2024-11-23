bb6d1d3bd070e86e3dda2c37099089ede4959293
andyhaha <489415746@qq.com>
2024-11-23 16:30:43 +0900

    upgrade libs dependency
    upgrade the dependencies of all

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.github.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.app"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
