e677b0125c3b02e5776993694c8fcc72e031e5c1
andyhaha <489415746@qq.com>
2024-11-30 12:56:23 +0900

    get GitHub API token from local.properties

--- libs/network/src/main/java/com/andy/network/AuthorizationHeaderInterceptor.kt
+++ libs/network/src/main/java/com/andy/network/AuthorizationHeaderInterceptor.kt
@@ -1,13 +1,12 @@
 package com.andy.network
 
-import androidx.multidex.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
 class AuthorizationHeaderInterceptor : Interceptor {
     companion object {
-        private const val GITHUB_TOKEN = BuildConfig.VERSION_CODE
+        private const val GITHUB_TOKEN = BuildConfig.API_TOKEN
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
